fed865c0c40335d9048649d2c4290d8e87469696
NikkiEllis <58333151+NikkiEllis@users.noreply.github.com>
2019-11-29 13:01:55 +0000

    Update head.js

--- src/head.js
+++ src/head.js
@@ -1,4 +1,4 @@
-const endpoint = 'https://www.jsonstore.io/6b6e3cd07f3aaecd8df25ab95875eb2462f00a7614f7205c2d5dc5229896ea98'
+const endpoint = 'https://www.jsonstore.io/76ec367f4ec71655ee943c489d27bec821f6a443de9b832b941327e625af7772'
 const ebox = document.getElementById('erbox')
 const fetchJSON = (a) => {
 	const f = new XMLHttpRequest()
